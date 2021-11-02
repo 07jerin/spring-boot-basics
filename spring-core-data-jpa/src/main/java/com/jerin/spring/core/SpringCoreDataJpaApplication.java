f56418dbfbf8ab9c4c82a4d7e070db070cf6cbc0
Jerin Joseph <jerin.joseph@oracle.com>
2021-11-02 23:55:52 +0530

    Spring core JPA

---
+++ spring-core-data-jpa/src/main/java/com/jerin/spring/core/SpringCoreDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.jerin.spring.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDataJpaApplication.class, args);
+	}
+
+}
---
+++ spring-core-data-jpa/src/test/java/com/jerin/spring/core/SpringCoreDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jerin.spring.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCoreDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
